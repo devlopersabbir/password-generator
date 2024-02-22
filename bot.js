1bb7a0aeac80042f10a3816ba048303285cf47b9
devlopersabbir <devlopersabbir@gmail.com>
2024-02-23 00:37:10 +0600

    [chore] automate updated

---
+++ bot.js
@@ -0,0 +1,10 @@
+import { simpleGit } from "simple-git";
+
+const filePath = "./index.ts";
+const ignore = "./.gitignore";
+const mainFile = "./bot.js";
+
+simpleGit()
+  .add([filePath, ignore, mainFile])
+  .commit("[chore] automate updated", { "--date": "2024-02-23" })
+  .push();
