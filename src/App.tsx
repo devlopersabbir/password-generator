47df7bea04a821de8994b7852d26243c209e7bce
devlopersabbir <devlopersabbir@gmail.com>
2024-03-03 01:20:34 +0600

    [init ✨] password generator project init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="container mx-auto flex justify-center items-center h-screen">
+      <h1>hello world</h1>
+    </div>
+  );
+};
+
+export default App;
