19a51cbd1f14e7e1dcdc7bcc03eebcb6ddae1704
devlopersabbir <devlopersabbir@gmail.com>
2024-03-04 22:34:12 +0600

    [feat ✨] ui for generation password

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import { Copy } from "lucide-react";
+import { Copy, RotateCw } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
+import { useEffect, useState } from "react";
+import { Slider } from "./components/ui/slider";
+import { Switch } from "./components/ui/switch";
 
 const App = () => {
+  const [generatedPassword, setGeneratedPassword] = useState<string>("234dfa");
+  const [passwordLenght, setPasswordLenght] = useState<number>(20);
+  const [numbers, setNumbers] = useState<boolean>(false);
+  const [spacial, setSpacial] = useState<boolean>(false);
+  const [uppercase, setUppercase] = useState<boolean>(false);
+  const [lowercase, setLowercase] = useState<boolean>(false);
+
+  function generate() {
+    console.log("fuck");
+  }
+  useEffect(() => {
+    generate();
+  }, [passwordLenght, numbers, spacial, uppercase, lowercase]);
   return (
     <div className="container mx-auto flex justify-center items-center h-screen">
-      <div className="max-w-lg p-10 rounded-lg shadow-lg flex justify-center items-center flex-col">
+      <div className="max-w-lg p-10 rounded-lg shadow-lg flex justify-center items-center flex-col gap-4">
         <img className="w-40 mb-2" src="/logo.gif" alt="logo" />
         <div className="flex gap-2 flex-col text-center justify-center items-center">
           <h1 className="text-3xl uppercase font-bold">password Generator</h1>
@@ -15,11 +31,76 @@ const App = () => {
         </div>
         {/* input & copy */}
         <div className="flex justify-between items-center w-full gap-4 mt-5">
-          <Input type="text" className="w-full rounded-full" />
-          <Button className="text-lg">
+          <div className="w-full rounded-full relative flex justify-center items-center">
+            <Input
+              value={generatedPassword}
+              disabled
+              type="text"
+              className="w-full rounded-full"
+            />
+            <RotateCw
+              onClick={generate}
+              size={24}
+              className="absolute right-3 font-bold top-2 cursor-pointer"
+            />
+          </div>
+          <Button
+            className="text-lg rounded-full"
+            onClick={async () =>
+              await navigator.clipboard.writeText(generatedPassword)
+            }
+          >
             <Copy className="mr-2" size={20} />
             Copy
           </Button>
+        </div>
+        {/* password length */}
+        <div className="flex flex-col justify-center items-center gap-4 w-full">
+          <label className="font-semibold text-base">
+            Password Length: {passwordLenght}
+          </label>
+          <Slider
+            onValueChange={(e) => console.log(e)}
+            max={100}
+            min={2}
+            step={1}
+            className="w-3/4"
+          />
+        </div>
+        {/* uppercase section */}
+        <div className="flex justify-between items-center w-full">
+          <label htmlFor="uppercase">uppercase</label>
+          <Switch
+            id="uppercase"
+            onCheckedChange={(e) => setUppercase(e)}
+            checked={uppercase}
+          />
+        </div>
+        {/* lowercase section */}
+        <div className="flex justify-between items-center w-full">
+          <label>lowercase</label>
+          <Switch
+            onCheckedChange={(e) => setLowercase(e)}
+            checked={lowercase}
+          />
+        </div>
+        {/* numbers section */}
+        <div className="flex justify-between items-center w-full">
+          <label htmlFor="numbers">numbers</label>
+          <Switch
+            onCheckedChange={(e) => setNumbers(e)}
+            id="numbers"
+            checked={numbers}
+          />
+        </div>
+        {/* spacial charector section */}
+        <div className="flex justify-between items-center w-full">
+          <label htmlFor="spacial">spacial charector</label>
+          <Switch
+            onCheckedChange={(e) => setSpacial(e)}
+            id="spacial"
+            checked={spacial}
+          />
         </div>
       </div>
     </div>
