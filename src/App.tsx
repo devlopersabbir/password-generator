062b0e6cba65835dea48d4fb3194385cef5f734c
devlopersabbir <devlopersabbir@gmail.com>
2024-03-05 21:26:54 +0600

    [feat 🎡] awesome UI

--- src/App.tsx
+++ src/App.tsx
@@ -24,87 +24,115 @@ const App = () => {
     generate();
   }, [passwordLenght, numbers, spacial, uppercase, lowercase]);
   return (
-    <div className="container mx-auto flex justify-center items-center h-screen">
-      <div className="max-w-lg p-10 rounded-lg shadow-lg flex justify-center items-center flex-col gap-4">
-        <img className="w-40 mb-2" src="/logo.gif" alt="logo" />
-        <div className="flex gap-2 flex-col text-center justify-center items-center">
-          <h1 className="text-3xl uppercase font-bold">password Generator</h1>
-          <p className="text-base">
-            Create strong and secure passwords to keep your account safe online
-          </p>
+    <div className="container mx-auto flex flex-col justify-center items-center h-screen">
+      {/* top bar */}
+      <div className="flex gap-0 flex-col text-center justify-center items-center">
+        <h1 className="text-4xl capitalize font-black">
+          Secure, Random Password Generator
+        </h1>
+        <p className="text-4xl font-normal">Need a password?</p>
+        <p className="text-base font-normal mt-3">
+          Create strong and secure passwords to keep your account safe online
+        </p>
+      </div>
+      {/* main container */}
+      <div className="flex justify-center items-center gap-5 mt-7">
+        {/* left side image */}
+        <div className="flex items-center justify-center w-1/2">
+          <img src="/img.svg" alt="image" className="w-3/4" />
         </div>
-        {/* input & copy */}
-        <div className="flex justify-between items-center w-full gap-4 mt-5">
-          <div className="w-full rounded-full relative flex justify-center items-center">
-            <Input
-              value={generatedPassword}
-              disabled
-              type="text"
-              className="w-full rounded-full text-lg disabled:font-bold disabled:text-zinc-900 font-semibold text-zinc-900 disabled:opacity-100"
-            />
-            <RotateCw
-              onClick={generate}
-              size={24}
-              className="absolute right-3 font-bold top-2 cursor-pointer"
-            />
+        {/* right side content */}
+        <div className="flex flex-col justify-center items-center w-1/2">
+          <div className="flex flex-col gap-3 justify-center items-center">
+            {/* output section && copy section */}
+            <div className="flex justify-between items-center w-full gap-4 mb-5">
+              <div className="w-full rounded-full relative flex justify-center items-center">
+                <Input
+                  value={generatedPassword}
+                  disabled
+                  type="text"
+                  className="w-full rounded-full text-lg disabled:font-bold disabled:text-zinc-900 font-semibold text-zinc-900 disabled:opacity-100"
+                />
+                <RotateCw
+                  onClick={generate}
+                  size={24}
+                  className="absolute right-3 font-bold top-2 cursor-pointer"
+                />
+              </div>
+              <Button
+                className="text-lg rounded-full"
+                onClick={async () =>
+                  await navigator.clipboard.writeText(generatedPassword)
+                }
+              >
+                <Copy className="mr-2" size={20} />
+                Copy
+              </Button>
+            </div>
+
+            {/* password length */}
+            <div className="flex flex-col justify-center items-center gap-4 w-full">
+              <label className="font-semibold text-base">
+                Password Length: {passwordLenght}
+              </label>
+              <Slider
+                onValueChange={(e) => setPasswordLenght(e)}
+                max={20}
+                min={2}
+                step={1}
+                className="w-3/4"
+              />
+            </div>
+            {/* bottom section */}
+            <div className="flex flex-col mt-7 w-full">
+              <p className="text-base font-semibold">Customize your password</p>
+              <div className="w-full bg-zinc-900 py-[1px] my-2" />
+            </div>
+            {/* uppercase section */}
+            <div className="flex justify-between items-center w-full">
+              <label htmlFor="uppercase" className="font-semibold capitalize">
+                uppercase
+              </label>
+              <Switch
+                id="uppercase"
+                onCheckedChange={(e) => setUppercase(e)}
+                checked={uppercase}
+              />
+            </div>
+            {/* lowercase section */}
+            <div className="flex justify-between items-center w-full">
+              <label className="font-semibold capitalize" htmlFor="lowercase">
+                lowercase
+              </label>
+              <Switch
+                id="lowercase"
+                onCheckedChange={(e) => setLowercase(e)}
+                checked={lowercase}
+              />
+            </div>
+            {/* numbers section */}
+            <div className="flex justify-between items-center w-full">
+              <label htmlFor="numbers" className="font-semibold capitalize">
+                numbers
+              </label>
+              <Switch
+                onCheckedChange={(e) => setNumbers(e)}
+                id="numbers"
+                checked={numbers}
+              />
+            </div>
+            {/* spacial charector section */}
+            <div className="flex justify-between items-center w-full">
+              <label htmlFor="spacial" className="font-semibold capitalize">
+                spacial charector
+              </label>
+              <Switch
+                onCheckedChange={(e) => setSpacial(e)}
+                id="spacial"
+                checked={spacial}
+              />
+            </div>
           </div>
-          <Button
-            className="text-lg rounded-full"
-            onClick={async () =>
-              await navigator.clipboard.writeText(generatedPassword)
-            }
-          >
-            <Copy className="mr-2" size={20} />
-            Copy
-          </Button>
-        </div>
-        {/* password length */}
-        <div className="flex flex-col justify-center items-center gap-4 w-full">
-          <label className="font-semibold text-base">
-            Password Length: {passwordLenght}
-          </label>
-          <Slider
-            onValueChange={(e) => setPasswordLenght(e)}
-            max={20}
-            min={2}
-            step={1}
-            className="w-3/4"
-          />
-        </div>
-        {/* uppercase section */}
-        <div className="flex justify-between items-center w-full">
-          <label htmlFor="uppercase">uppercase</label>
-          <Switch
-            id="uppercase"
-            onCheckedChange={(e) => setUppercase(e)}
-            checked={uppercase}
-          />
-        </div>
-        {/* lowercase section */}
-        <div className="flex justify-between items-center w-full">
-          <label>lowercase</label>
-          <Switch
-            onCheckedChange={(e) => setLowercase(e)}
-            checked={lowercase}
-          />
-        </div>
-        {/* numbers section */}
-        <div className="flex justify-between items-center w-full">
-          <label htmlFor="numbers">numbers</label>
-          <Switch
-            onCheckedChange={(e) => setNumbers(e)}
-            id="numbers"
-            checked={numbers}
-          />
-        </div>
-        {/* spacial charector section */}
-        <div className="flex justify-between items-center w-full">
-          <label htmlFor="spacial">spacial charector</label>
-          <Switch
-            onCheckedChange={(e) => setSpacial(e)}
-            id="spacial"
-            checked={spacial}
-          />
         </div>
       </div>
     </div>
